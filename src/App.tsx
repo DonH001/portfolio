58e9dc19df752237d332567f7d5d7a9c49dcf8d2
Hector Sanchez <donhsanchez001@gmail.com>
2025-06-11 12:35:34 +0800

    fix anchor links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Home from "./pages/Home";
